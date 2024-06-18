0244dfd55719aaf3168943a9b3bf5877c1500045
Karsten Beck <s3912792@student.rmit.edu.au>
2024-06-18 11:01:42 +1000

    code update remove app.use(express.public)

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ const app = express();
 app.use('*', cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
 
 mongoose.connect(process.env.DATABASE_URL, {
